83811af55987f763c6ccd87750637953ee00a0b8
ash7811 <ash7811@rit.edu>
2024-09-17 19:46:11 -0400

    in progress

---
+++ src/htmlResponses.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+
+const getIndex = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+module.exports = {
+  getIndex,
+};
